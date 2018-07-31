0fdcec2f74d652b000dc6aa104bf3364f1732e68
sofimalko <sofimalko@gmail.com>
2018-08-01 02:21:15 +0300

    Practice1

---
+++ 1.js
@@ -0,0 +1,21 @@
+console.log(2+6); // l1.t1
+
+let x = "weather" 
+console.log("Test_" + x); // l1.t2
+
+let firstname = "Sofiya";
+let lastname = "Malko";
+let age = 24;
+console.log(firstname + " " + lastname + " is " + age + " years old."); // l1.t3
+
+let a = 50;
+console.log(a>100); // l1.t4
+
+let b = 6;
+let c = 10;
+let d;
+console.log(a>b && a>c); // l1.t5
+
+console.log (a==50 || b==50 || a+b==50); // l1.t6
+
+console.log (b%3==0 || b%7==0); // l1.t7
